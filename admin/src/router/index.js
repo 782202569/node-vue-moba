f30e2742c4957e6fb229a82bb6ffc1c8e8cfffb4
guohuanlei@utry.cn <guohuanlei@utry.cn>
2020-11-29 21:47:18 +0800

    update: 迭代更新

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -2,9 +2,21 @@ import Vue from "vue"
 import VueRouter from "vue-router"
 //mport Home from "../views/Home.vue";
 import Main from "../views/Main.vue"
+// 分类
 import CategoryEdit from "../views/CategoryEdit.vue"
 import CategoryList from "../views/CategoryList.vue"
-
+// 物品
+import itemEdit from "../views/itemEdit.vue"
+import itemList from "../views/itemList.vue"
+// 英雄
+import HeroEdit from "../views/HeroEdit.vue"
+import HeroList from "../views/HeroList.vue"
+// 文章
+import ArticleEdit from "../views/ArticlesEdit.vue"
+import ArticleList from "../views/ArticlesList.vue"
+// 广告位
+import AdEdit from "../views/AdEdit.vue"
+import AdList from "../views/AdList.vue"
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -25,6 +37,62 @@ const router = new VueRouter({
         {
           path: "/categories/edit/:id",
           component: CategoryEdit,
+          props: true,
+        },
+
+        {
+          path: "/items/create",
+          component: itemEdit,
+        },
+        {
+          path: "/items/list",
+          component: itemList,
+        },
+        {
+          path: "/items/edit/:id",
+          component: itemEdit,
+          props: true,
+        },
+
+        {
+          path: "/heroes/create",
+          component: HeroEdit,
+        },
+        {
+          path: "/heroes/list",
+          component: HeroList,
+        },
+        {
+          path: "/heroes/edit/:id",
+          component: HeroEdit,
+          props: true,
+        },
+
+        {
+          path: "/Articles/create",
+          component: ArticleEdit,
+        },
+        {
+          path: "/Articles/list",
+          component: ArticleList,
+        },
+        {
+          path: "/Articles/edit/:id",
+          component: ArticleEdit,
+          props: true,
+        },
+
+        {
+          path: "/Ads/create",
+          component: AdEdit,
+        },
+        {
+          path: "/Ads/list",
+          component: AdList,
+        },
+        {
+          path: "/Ads/edit/:id",
+          component: AdEdit,
           props: true,
         },
       ],
--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -2,6 +2,17 @@
   <div class="about">
     <h1>{{ id ? "编辑" : "新建" }}分类</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="上级分类">
+        <el-select v-model="model.parent">
+          <el-option
+            v-for="item in parents"
+            :key="item._id"
+            :label="item.name"
+            :value="item._id"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
@@ -22,17 +33,36 @@ export default {
       model: {
         name: "",
       },
+      parents: [],
     }
   },
   methods: {
     async save() {
-      await this.$http.post("categories", this.model)
+      if (this.id) {
+        await this.$http.put(`rest/categories/${this.id}`, this.model)
+      } else {
+        await this.$http.post("rest/categories", this.model)
+      }
       this.$router.push("/categories/list")
       this.$message({
         type: "success",
         message: "保存成功",
       })
     },
+    async fetch() {
+      const res = await this.$http.get(`rest/categories/${this.id}`)
+      console.log(res)
+      this.model = res.data
+    },
+    async fetchParents() {
+      const res = await this.$http.get(`rest/categories`)
+      console.log(res)
+      this.parents = res.data
+    },
+  },
+  created() {
+    this.fetchParents()
+    this.id && this.fetch()
   },
 }
 </script>
