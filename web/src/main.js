1d0b3e9db9995da0f8c2f0f9f05493ef91abe421
guohuanlei@utry.cn <guohuanlei@utry.cn>
2020-12-11 16:33:00 +0800

    update:移动端开发

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue"
+import App from "./App.vue"
+
+import "./assets/scss/style.scss"
+import router from "./router"
+
+import VueAwesomeSwiper from "vue-awesome-swiper"
+import "swiper/swiper-bundle.css"
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app")
---
+++ web/src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <swiper ref="mySwiper" :options="swiperOptions">
+      <swiper-slide>
+        <img class="w-100 h-100" src="../assets/images/gg.jpeg" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100 h-100" src="../assets/images/hh.jpeg" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100 h-100" src="../assets/images/ll.jpeg" />
+      </swiper-slide>
+      <div
+        class="swiper-pagination pagination-home text-right px-3 pb-2"
+        slot="pagination"
+      ></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: ".swiper-pagination",
+        },
+      },
+    }
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.$swiper
+    },
+  },
+  mounted() {
+    this.swiper.slideTo(3, 1000, false)
+  },
+}
+</script>
+<style lang="scss">
+@import "../assets/scss/_variables.scss";
+
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.1538rem;
+    background: map-get($colors, "white");
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, "info");
+    }
+  }
+}
+</style>
