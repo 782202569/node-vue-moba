2554fa91eaca277b72e27327bd3218a71aff07bb
guohuanlei@utry.cn <guohuanlei@utry.cn>
2020-12-12 14:44:05 +0800

    update: 移动端开发

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -15,18 +15,32 @@
         slot="pagination"
       ></div>
     </swiper>
+    <!-- end of swiper -->
+    <div class="nav-icons bg-white mt-3 text-center pt-3 text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="n in 10" :key="n">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
+      </div>
+    </div>
+    <!-- end of nav icons -->
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
+  name: 'Home',
   components: {},
   data() {
     return {
       swiperOptions: {
         pagination: {
-          el: ".swiper-pagination",
+          el: '.swiper-pagination',
         },
       },
     }
@@ -42,15 +56,27 @@ export default {
 }
 </script>
 <style lang="scss">
-@import "../assets/scss/_variables.scss";
+@import '../assets/scss/_variables.scss';
 
 .pagination-home {
   .swiper-pagination-bullet {
     opacity: 1;
     border-radius: 0.1538rem;
-    background: map-get($colors, "white");
+    background: map-get($colors, 'white');
     &.swiper-pagination-bullet-active {
-      background: map-get($colors, "info");
+      background: map-get($colors, 'info');
+    }
+  }
+}
+
+.nav-icons {
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-right: 1px solid $border-color;
+    &:nth-child(4n) {
+      border-right: none;
     }
   }
 }
